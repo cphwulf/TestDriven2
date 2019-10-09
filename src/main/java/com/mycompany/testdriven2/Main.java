0cd7bac01a49fc89462e5ada0bf94690881bebf4
thor <thor@cphbusiness.dk>
2019-10-09 14:09:14 +0200

    First commit

--- src/main/java/com/mycompany/testdriven2/Main.java
+++ src/main/java/com/mycompany/testdriven2/Main.java
@@ -4,6 +4,7 @@ package com.mycompany.testdriven2;
 public class Main {
     public static void main(String[] args) {
         ConsoleIO io = new SystemIO();
+	// test for nyt hook
         CalculatorUI calculatorUI = new  CalculatorUI(io);
         calculatorUI.start();
     }
