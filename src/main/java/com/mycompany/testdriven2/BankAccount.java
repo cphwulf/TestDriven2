3d1b2f581a3419e61006f1f1d6e0d080d59ca1da
thor <thor@cphbusiness.dk>
2019-10-09 14:35:11 +0200

    first

--- src/main/java/com/mycompany/testdriven2/BankAccount.java
+++ src/main/java/com/mycompany/testdriven2/BankAccount.java
@@ -16,7 +16,7 @@ public class BankAccount {
     public void withdraw(int i) {
         balance-=i;
         if(balance<0)
-            balance-=2;
+            balance-=1;
     }
 
     void transfer(BankAccount accountTo, int i) {
