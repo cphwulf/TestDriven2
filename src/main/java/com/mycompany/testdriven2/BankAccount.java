cf4380d7d42de2bfd450852a16054531e26e5d89
thor <thor@cphbusiness.dk>
2019-10-09 15:05:38 +0200

    new

--- src/main/java/com/mycompany/testdriven2/BankAccount.java
+++ src/main/java/com/mycompany/testdriven2/BankAccount.java
@@ -16,7 +16,7 @@ public class BankAccount {
     public void withdraw(int i) {
         balance-=i;
         if(balance<0)
-            balance-=6;
+            balance-=76;
     }
 
     void transfer(BankAccount accountTo, int i) {
